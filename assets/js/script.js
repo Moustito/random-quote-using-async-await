9709febaaf67e966c01eda77ff374632ce13029d
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-08-17 12:25:23 +0200

    css- style finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,18 @@
-fetch("https://thatsthespir.it/api").then((res) => {
-  if (res.ok) {
-    res.json().then((data) => {quote(data)});
-  } else {
-    console.log("ERROR");
+async function fetchQuotes() {
+  try {
+      const response = await fetch("https://thatsthespir.it/api");
+      const data = await response.json();
+      document.getElementById("quote").innerHTML = data.quote;
+      document.getElementById("author").innerHTML = data.author;
+      let photo = document.getElementById("photo")
+      console.log(data.photo);
+      data.photo == ""? photo.src = "assets/image/user.png": photo.src = data.photo
+      document.getElementById("photo").alt = data.author;
+  } catch (error) {
+      alert("Error", error);
   }
-});
-
-function quote(data) {
-    let main = document.querySelector("main");
-      let section = document.createElement("section");
-      main.appendChild(section);
-
-      let article__quote = document.createElement("article");
-      section.appendChild(article__quote);
-      let quote = document.createTextNode(data.quote);
-      article__quote.appendChild(quote);
-      article__quote.id = "quote";
-
-      let article__author = document.createElement("article");
-      section.appendChild(article__author);
-      let author = document.createTextNode(" -" + data.author);
-      article__author.appendChild(author);
-      article__author.id = "author";
-
-      let div = document.createElement("div");
-      section.appendChild(div);
-      let image = document.createElement("img");
-      div.appendChild(image);
-      image.src = data.photo;
-      div.id = "div__image";
-
-      let button = document.createElement("button");
-      section.appendChild(button);
-      let buttonText = document.createTextNode("button");
-      button.appendChild(buttonText);
-      button.id = "button";
-
-      button.addEventListener("click", () => {
-        location.reload()
-    });
 }
+document.getElementById("nquote").addEventListener("click", () => {
+  fetchQuotes();
+});
+fetchQuotes();
